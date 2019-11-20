de404daa06c0523fcedad214dbf0f53ffabd4a7d
Andrzej Kapolka <drzej.k@gmail.com>
2019-11-20 15:06:05 -0800

    Working on hover/select highlights.

--- src/components.ts
+++ src/components.ts
@@ -1,10 +1,32 @@
+import {Object3D} from "three"
+
+import {Value} from "tfw/core/react"
 import {Hover} from "tfw/engine/game"
 import {TypeScriptComponent, registerConfigurableType} from "tfw/engine/typescript/game"
+import {ThreeObjectComponent} from "tfw/engine/typescript/three/render"
 import {Keyboard} from "tfw/input/keyboard"
 
 import {selection} from "./ui/model"
 
 class Selector extends TypeScriptComponent {
+
+  awake () {
+    this._disposer.add(
+      Value
+        .join2(
+          this.gameObject
+            .getProperty<ThreeObjectComponent|undefined>("hoverable")
+            .switchMap(hoverable => {
+              if (!hoverable) return Value.constant<[number, Object3D|undefined]>([0, undefined])
+              return Value.join2(hoverable.hovers.sizeValue, hoverable.objectValue)
+            }),
+          selection.hasValue(this.gameObject.name),
+        )
+        .onValue(([[hovered, object], selected]) => {
+
+        })
+    )
+  }
 
   onPointerDown (identifier :number, hover :Hover) {
     if (Keyboard.instance.getKeyState(17).current) { // control
